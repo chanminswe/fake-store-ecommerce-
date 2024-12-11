bd292d565e486301154b0f4a80ea7ac6af2ff4bf
chanminswe <chanswe67@gmail.com>
2024-12-11 21:04:42 +0630

    tryng. on the responsive nav bar shameless ly

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,55 @@
 import React from "react";
-import "./NavBar.css";
 import { Link } from "react-router-dom";
-
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <header className="navigation-header">
-      <h2>MTA</h2>
-      <nav className="navigation-routes">
-        <Link to='/'>Home</Link>
-        <Link to='/shop'>Shops</Link>
-        <p>Review</p>
-        <p>About</p>
-      </nav>
-    </header>
+    <nav className="nav-container">
+      <ul className="nav-menu sidebar">
+        <li className="nav-option">
+          <Link className="link" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-option">
+          <Link className="link" to="/">
+            Shop
+          </Link>
+        </li>
+        <li className="nav-option">
+          <Link className="link" to="/">
+            About
+          </Link>
+        </li>
+        <li className="nav-option">
+          <Link className="link" to="/">
+            Cart
+          </Link>
+        </li>
+      </ul>
+      <ul className="nav-menu">
+        <li className="nav-option">
+          <Link className="link" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-option">
+          <Link className="link" to="/">
+            Shop
+          </Link>
+        </li>
+        <li className="nav-option">
+          <Link className="link" to="/">
+            About
+          </Link>
+        </li>
+        <li className="nav-option">
+          <Link className="link" to="/">
+            Cart
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
