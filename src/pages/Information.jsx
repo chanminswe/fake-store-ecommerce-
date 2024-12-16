83e489b9033a039be4528c3f409515f501025780
chanminswe <chanswe67@gmail.com>
2024-12-16 23:43:17 +0630

    adding new stuff on information page

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "../styles/pages/Information.css";
 
 function Information() {
+  const { id } = useParams();
 
-    const {id} = useParams();
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setItem(data);
+      });
+  }, []);
 
   return (
-    <div>
+    <div className="information-container">
+      <figure className="image-container">
+        <img 
+        className="image-information"
+        src={item.image} />
+      </figure>
 
+      <div className="item-informations">
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Information
+export default Information;
